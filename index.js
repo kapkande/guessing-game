21d05036393351e48f0b7d9b1c34d319646ee6c6
kapkande <kap.kande@gmail.com>
2022-08-17 02:19:39 +0300

    init: start

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 const GuessingGame = require('./src/guessing-game.js');
 
+function onEnterLabel(even) {
+    const input = enterLabel.value
+    if (input) {
+
+        enterLabel.style.display = 'none';
+        invisible.style.display = 'inline-block';
+        invisible.textContent = input
+        todoBlock.style.display = 'flex';
+        text.style.display = 'none';
+
+        textt.style.display = 'block';
+    }
+}
+const onClickLabel = (even) => {
+    textt.style.display = 'none';
+    const target = even.target;
+    enterLabel.style.display = 'block';
+    text.style.display = 'block';
+    todoBlock.style.display = 'none';
+    invisible.style.display = 'none';
+    enterLabel.value = ''
+
+}
 window.game = new GuessingGame();
